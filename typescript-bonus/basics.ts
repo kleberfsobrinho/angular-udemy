59e62451e2e7aff92939682ae928a4e868d14a3b
Kleber Sobrinho <kleber.sobrinho@ccc.ufcg.edu.br>
2021-08-12 11:16:28 -0700

    add TypeScript Introduction

---
+++ typescript-bonus/basics.ts
@@ -0,0 +1,9 @@
+// Primitives: number, string, boolean
+// More complex types: arrays, objects
+// Function types, parameters
+
+// Primitives
+
+let age: number;
+let userName: string;
+let isInstructor = true;
