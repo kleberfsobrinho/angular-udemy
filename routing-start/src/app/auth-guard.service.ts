7cbfbf494dbdba7ed029470c118deb19f907eeee
Kleber Sobrinho <kleber.sobrinho@ccc.ufcg.edu.br>
2021-09-02 07:35:22 -0700

    Protecting Child Routes with canActivateChild

--- routing-start/src/app/auth-guard.service.ts
+++ routing-start/src/app/auth-guard.service.ts
@@ -2,6 +2,7 @@ import { Injectable } from "@angular/core";
 import { 
     ActivatedRouteSnapshot, 
     CanActivate, 
+    CanActivateChild, 
     Router, 
     RouterStateSnapshot } from "@angular/router";
 import { Observable } from "rxjs/Observable";
@@ -9,11 +10,11 @@ import { AuthService } from "./auth.service";
 
 @Injectable()
 
-export class AuthGuard implements CanActivate {
+export class AuthGuard implements CanActivate, CanActivateChild {
     constructor(private authService: AuthService, private router: Router) { }
       
     canActivate(route: ActivatedRouteSnapshot, 
-        state: RouterStateSnapshot): Observable<boolean> | Promise<boolean> | boolean{
+        state: RouterStateSnapshot): Observable<boolean> | Promise<boolean> | boolean {
         return this.authService.isAuthenticated().then(
             (authenticated: boolean) => {
                 if(authenticated) {
@@ -23,5 +24,10 @@ export class AuthGuard implements CanActivate {
                 }
             }
         )
+    }
+
+    canActivateChild (route: ActivatedRouteSnapshot, 
+    state: RouterStateSnapshot): Observable<boolean> | Promise<boolean> | boolean {
+        return this.canActivate(route, state);
     }
 }
