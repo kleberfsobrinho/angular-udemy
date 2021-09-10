6fb0a748529be9a72f78600c10b022f6fba87cb0
Kleber Sobrinho <kleber.sobrinho@ccc.ufcg.edu.br>
2021-09-10 06:46:42 -0700

    Adding Editing Routes

---
+++ my-first-app/src/app/recipes/recipe-edit/recipe-edit.component.ts
@@ -0,0 +1,18 @@
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+
+@Component({
+  selector: 'app-recipe-edit',
+  templateUrl: './recipe-edit.component.html',
+  styleUrls: ['./recipe-edit.component.css']
+})
+export class RecipeEditComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+
+
+}
